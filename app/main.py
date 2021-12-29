5f423bc258285940d1df7f2032ffb3e00c6ce7ce
Yuma-Nakamura1 <Yuma.Nakamura1@ibm.com>
2021-12-29 17:53:19 +0900

    delete debug code

--- app/main.py
+++ app/main.py
@@ -122,37 +122,3 @@ def get_logs(community_id: int):
     records = [r.to_json() for r in records_obj]
     json_item = jsonable_encoder({"list": records})
     return JSONResponse(content=json_item)
-
-
-#  debugging below
-
-
-@app.post("/debug/")
-def debug(item: RequestItem):
-    return JSONResponse(content={})
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool = None
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World", "URI": SQLALCHEMY_DATABASE_URI}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.post("/items/")
-def post_item(item: Item):
-    return {"item_name": item.name, "item_price": item.price}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
